268f75c95066d2798614f5bb51dc43eb3b7b172d
bibhuti9 <bibhutiswain990@gmail.com>
2023-03-12 11:22:17 +0530

    first commit

---
+++ App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+export default function App() {
+  return (
+    <View>
+      <Text>Hello World</Text>
+      <Text>Panchu nahak</Text>
+    </View>
+  );
+}
+
+/*
+
+return Component
+
+<View></View>
+<Text></Text>
+<Image/>
+<TextInput/>
+<Button/> | <TouchableOpacity></TouchableOpacity>
+
+
+*/
